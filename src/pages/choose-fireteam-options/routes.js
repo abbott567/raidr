b0f05471fea0487d240f8b0c386abfcaef191873
Craig Abbott <craigabbott.digitaldwp@gmail.com>
2016-11-03 16:21:09 +0000

    Redirects based on selection

--- src/pages/choose-fireteam-options/routes.js
+++ src/pages/choose-fireteam-options/routes.js
@@ -1,10 +1,25 @@
 const express = require('express');
 const template = require('./template.marko');
+const {validate} = require('./functions');
 
 const router = new express.Router();
 
 router.get('/', (req, res) => {
   template.render({}, res);
+});
+
+router.post('/', (req, res) => {
+  const gameType = req.body['game-type'];
+  const errors = validate();
+  if (errors.length === 0) {
+    if (gameType === 'solo') {
+      res.redirect('/which-raid-solo');
+    } else {
+      res.redirect('/which-raid-team');
+    }
+  } else {
+    template.render({errors}, res);
+  }
 });
 
 module.exports = router;
