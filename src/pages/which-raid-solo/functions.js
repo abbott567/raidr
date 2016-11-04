77f211da065b24eb8195bb2d0f2778334c483c36
Craig Abbott <craigabbott.digitaldwp@gmail.com>
2016-11-04 08:47:20 +0000

    Removed language from which-raid-solo

--- src/pages/which-raid-solo/functions.js
+++ src/pages/which-raid-solo/functions.js
@@ -1,16 +1,12 @@
 module.exports = {
   validate: req => {
     const raid = req.body.raid;
-    const language = req.body.language;
     const errors = [];
 
     if (!raid) {
       errors.push('Choose a raid');
     }
 
-    if (!language) {
-      errors.push('Choose a language');
-    }
     return errors;
   }
 };
