5cd7e3bbe4e81d18364e8ef420ef5d368a07d57a
Craig Abbott <craigabbott.digitaldwp@gmail.com>
2016-11-06 22:00:21 +0000

    Added errors for awaiting-orbit incase some info is missing

--- src/pages/awaiting-orbit/routes.js
+++ src/pages/awaiting-orbit/routes.js
@@ -4,7 +4,13 @@ const template = require('./template.marko');
 const router = new express.Router();
 
 router.get('/', (req, res) => {
-  template.render({}, res);
+  const player = req.cookies.player;
+
+  if (player && player.character && player.completedRaids) {
+    template.render({}, res);
+  } else {
+    template.render({errors: []}, res);
+  }
 });
 
 module.exports = router;
