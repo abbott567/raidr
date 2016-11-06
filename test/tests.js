f1ef7c42ef43aacc2d98e075175a129bedb2c494
Craig Abbott <craigabbott.digitaldwp@gmail.com>
2016-11-06 17:50:52 +0000

    Set test timeouts to 5 secs

--- test/tests.js
+++ test/tests.js
@@ -1,26 +1,30 @@
 process.env.NODE_ENV = 'test';
 
 require('../bin/www');
-
 /* eslint-disable no-undef */
 
-describe('enter-gamertag', () => {
+describe('enter-gamertag', function () {
+  this.timeout(5000);
   require('../src/pages/enter-gamertag/tests');
 });
 
-describe('choose-fireteam-options', () => {
+describe('choose-fireteam-options', function () {
+  this.timeout(5000);
   require('../src/pages/choose-fireteam-options/tests');
 });
 
-describe('find-players', () => {
+describe('find-players', function () {
+  this.timeout(5000);
   require('../src/pages/find-players/tests');
 });
 
-describe('find-a-team', () => {
+describe('find-a-team', function () {
+  this.timeout(5000);
   require('../src/pages/find-a-team/tests');
 });
 
-describe('show-raid-team', () => {
+describe('show-raid-team', function () {
+  this.timeout(5000);
   require('../src/pages/show-raid-team/tests');
 });
 
