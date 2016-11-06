8065b9b27e488ece7cc81bd2421a1a493553dea7
Craig Abbott <craigabbott.digitaldwp@gmail.com>
2016-11-06 22:42:34 +0000

    Uninstalled Serve-Favicon

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const express = require('express');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
-// const favicon = require('serve-favicon');
 
 // Config
 const db = require('./config/db-connect');
@@ -18,11 +17,10 @@ db.once('open', () => {
 
 const app = express();
 
-// uncomment after placing your favicon in /public
-// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 if (process.env.NODE_ENV !== 'test') {
   app.use(logger('dev'));
 }
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
