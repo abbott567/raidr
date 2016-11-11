9832631aa35224ddb011e1b9714dcc6bae8fb12b
Craig Abbott <craigabbott.digitaldwp@gmail.com>
2016-11-11 13:14:25 +0000

    Removed old routes

--- app.js
+++ app.js
@@ -27,16 +27,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
 app.use('/', require('./src/pages/index/routes'));
-app.use('/enter-gamertag', require('./src/pages/enter-gamertag/routes'));
-app.use('/awaiting-orbit', require('./src/pages/awaiting-orbit/routes'));
-app.use('/choose-fireteam-options', require('./src/pages/choose-fireteam-options/routes'));
-app.use('/find-players', require('./src/pages/find-players/routes'));
-app.use('/await-response', require('./src/pages/await-response/routes'));
-app.use('/find-a-team', require('./src/pages/find-a-team/routes'));
-app.use('/show-raid-team', require('./src/pages/show-raid-team/routes'));
-app.use('/await-decision', require('./src/pages/await-decision/routes'));
-app.use('/success-you-were-accepted', require('./src/pages/success-you-were-accepted/routes'));
-app.use('/invite-player-to-your-game', require('./src/pages/invite-player-to-your-game/routes'));
 
 // Routes for test API
 app.use('/api', require('./test/api-routes'));
