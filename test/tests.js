d346194e4f8e978c94af49ba07ed1f1db0d9c0b6
Craig Abbott <craigabbott.digitaldwp@gmail.com>
2016-11-17 19:05:26 +0000

    Set up test db for tests

--- test/tests.js
+++ test/tests.js
@@ -2,6 +2,9 @@ process.env.NODE_ENV = 'test';
 process.env.PORT = 8080;
 require('../bin/www');
 
+// Database setup
+require('./database-config/setup');
+
 // Validation Tests
 require('./validation-tests/enter-gamer-tag');
 require('./validation-tests/find-players-or-game');
