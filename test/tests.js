ead53c395c854661a0ebd30e41bada74ed871b50
Craig Abbott <craigabbott.digitaldwp@gmail.com>
2016-11-17 20:46:19 +0000

    Updated tests

--- test/tests.js
+++ test/tests.js
@@ -1,14 +1,32 @@
 process.env.NODE_ENV = 'test';
 process.env.PORT = 8080;
+
 require('../bin/www');
+const mongoose = require('mongoose');
 
-// Database setup
-require('./database-config/setup');
+const Game = mongoose.model('Game');
+mongoose.Promise = global.Promise;
 
-// Validation Tests
-require('./validation-tests/enter-gamer-tag');
-require('./validation-tests/find-players-or-game');
-require('./validation-tests/create-a-game');
-require('./validation-tests/find-a-game');
+describe('Validation tests', () => {
+  before(done => {
+    Game.remove({})
+    .then(() => {
+      done();
+    })
+    .catch(err => {
+      console.log(err);
+    });
+  });
+
+  after(done => {
+    done();
+  });
+
+  // Validation Tests
+  require('./validation-tests/enter-gamer-tag');
+  require('./validation-tests/find-players-or-game');
+  require('./validation-tests/create-a-game');
+  require('./validation-tests/find-a-game');
+});
 
 /* eslint-enable max-nested-callbacks */
--- test/validation-tests/create-a-game.js
+++ test/validation-tests/create-a-game.js
@@ -1,11 +1,17 @@
 const Browser = require('zombie');
+const mongoose = require('mongoose');
+
+const Game = mongoose.model('Game');
 
 /* eslint-disable max-nested-callbacks */
 describe('- Create a game screen', () => {
   let browser;
 
   before(done => {
-    browser = new Browser();
+    browser = new Browser({
+      waitFor: 100000,
+      maxWait: 800000
+    });
     browser.visit(`http://localhost:${process.env.PORT}`)
     .then(() => {
       browser.fill('#gamertag', 'abbott567');
@@ -37,6 +43,17 @@ describe('- Create a game screen', () => {
     it('should have element select#players', done => {
       browser.assert.elements('input[name="players"]', 5);
       done();
+    });
+  });
+
+  describe('-> Form submitted', done => {
+    it('should redirect to await-players on submit', () => {
+      browser.choose('#one');
+      browser.pressButton('button[type="submit"]');
+      browser.wait(() => {
+        browser.assert.text('h2', 'AWAIT PLAYERS');
+        done();
+      });
     });
   });
 });
